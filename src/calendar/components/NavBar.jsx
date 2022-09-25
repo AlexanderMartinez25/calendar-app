7c1af3986fbe5b04ba957b020772e3d81b4d3e10
Alexander <alexander.martinez@lapatota.cl>
2022-09-24 23:09:38 -0300

    LoginScreen y Navbar

---
+++ src/calendar/components/NavBar.jsx
@@ -0,0 +1,17 @@
+export const NavBar = () => {
+  return (
+    <div className="navbar navbar-dark bg-dark mb-4 px-4 text-white">
+      <span>
+        <i className="fas fa-calendar-alt"></i>
+        &nbsp;
+        Alexander
+      </span>
+
+      <button className="btn btn-outline-danger">
+        <i className="fas fa-sign-out-alt"></i>
+        &nbsp;
+        <span>Salir</span>
+      </button>
+    </div>
+  )
+}
