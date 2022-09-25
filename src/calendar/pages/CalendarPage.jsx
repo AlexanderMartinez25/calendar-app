24a066fef5dbd295f14531282faa1ada40fceed8
Alexander <alexander.martinez@lapatota.cl>
2022-09-25 16:59:07 -0300

    Escuchar eventos del calendario

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Calendar } from 'react-big-calendar'
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
@@ -21,6 +22,10 @@ const events = [
 ];
 
 export const CalendarPage = () => {
+
+  const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'week');
+  console.log(lastView);
+
   const eventStyleGetter = (event, start, end, isSelected) => {
     // console.log({ event, start, end, isSelected });
     const style = {
@@ -32,6 +37,17 @@ export const CalendarPage = () => {
     return { style }
   }
 
+  const onDobleClick = (event) => {
+    console.log({ doubleClick: event });
+  }
+
+  const onSelect = (event) => {
+    console.log({ click: event });
+  }
+
+  const onViewChanged = (event) => {
+    localStorage.setItem('lastView', event)
+  }
 
   return (
     <>
@@ -40,6 +56,7 @@ export const CalendarPage = () => {
         culture='es'
         localizer={localizer}
         events={events}
+        defaultView={lastView}
         startAccessor="start"
         endAccessor="end"
         style={{ height: 'calc( 100vh - 80px )' }}
@@ -48,6 +65,9 @@ export const CalendarPage = () => {
         components={{
           event: CalendarEvent
         }}
+        onDoubleClickEvent={onDobleClick}
+        onSelectEvent={onSelect}
+        onView={onViewChanged}
       />
     </>
   )
