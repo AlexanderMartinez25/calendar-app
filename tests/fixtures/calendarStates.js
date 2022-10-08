12499b966e8825d4e9085b0d67e3f1a4f49f1673
Alexander <alexander.martinez@lapatota.cl>
2022-10-08 15:17:00 -0300

    Calendar Fixtures

---
+++ tests/fixtures/calendarStates.js
@@ -0,0 +1,34 @@
+export const events = [
+  {
+    id: 1,
+    start: new Date("2022-10-08 15:12:00"),
+    end: Date("2022-10-08 17:12:00"),
+    title: "Cumpleaños de Alex",
+    notes: "Alguna nota",
+  },
+  {
+    id: 1,
+    start: new Date("2022-11-02 15:12:00"),
+    end: Date("2022-11-02 17:12:00"),
+    title: "Cumpleaños de Fátima",
+    notes: "Alguna nota Fátima",
+  },
+];
+
+export const initialState = {
+  isLoadingEvents: true,
+  events: [],
+  activeEvent: null,
+};
+
+export const calendarWidthEventsState = {
+  isLoadingEvents: false,
+  events: [...events],
+  activeEvent: null,
+};
+
+export const calendarWidthActiveEventState = {
+  isLoadingEvents: false,
+  events: [...events],
+  activeEvent: { ...events[0] },
+};
